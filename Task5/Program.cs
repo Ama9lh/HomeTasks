ef4dbd0e2784162164fa805bb0ef9467f5406915
Denis <kummerd65@gmail.com>
2023-01-13 14:59:14 +0600

    add Task7

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -7,3 +7,13 @@ while (m <= n)
     Console.Write($"{m} ");
     m = m + 1; // m++;
 }
+
+
+/* Task 7
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n % 10)*/
+
+
